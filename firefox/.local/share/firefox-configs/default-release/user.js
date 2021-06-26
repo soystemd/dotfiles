c0abacfc0b07e475600c9da63a8a534633c88598
ehsan gn <ehsangn@protonmail.ch>
2021-06-26 20:35:32 +0430

    add a lot more options to firefox's user.js

--- firefox/.local/share/firefox-configs/default-release/user.js
+++ firefox/.local/share/firefox-configs/default-release/user.js
@@ -2,219 +2,245 @@
  * ===    practical prefs     ===
  * ============================== */
 
-/* cache directory ***/
+/* cache directory */
 user_pref("browser.cache.disk.parent_directory", "/run/user/1000/firefox");
 
-/* scroll speed ***/
+/* interval for saving session data to restore if firefox crashes.
+ * default is 15000 milliseconds which is too short. */
+user_pref("browser.sessionstore.interval", 100000);
+
+/* scroll speed */
 user_pref("mousewheel.default.delta_multiplier_x", 150);
 user_pref("mousewheel.default.delta_multiplier_y", 150);
 
-/* smooooooth scroll ***/
-/*
-user_pref("general.smoothScroll.lines.durationMaxMS", 125);
-user_pref("general.smoothScroll.lines.durationMinMS", 125);
-user_pref("general.smoothScroll.mouseWheel.durationMaxMS", 200);
-user_pref("general.smoothScroll.mouseWheel.durationMinMS", 100);
-user_pref("general.smoothScroll.msdPhysics.enabled", true);
-user_pref("general.smoothScroll.other.durationMaxMS", 125);
-user_pref("general.smoothScroll.other.durationMinMS", 125);
-user_pref("general.smoothScroll.pages.durationMaxMS", 125);
-user_pref("general.smoothScroll.pages.durationMinMS", 125);
-*/
-
-/* additionhal mousewheel smooooooth scroll settings ***/
-/*
-user_pref("mousewheel.min_line_scroll_amount", 30);
-user_pref("mousewheel.system_scroll_override_on_root_content.enabled", true);
-user_pref("mousewheel.system_scroll_override_on_root_content.horizontal.factor", 175);
-user_pref("mousewheel.system_scroll_override_on_root_content.vertical.factor", 175);
-user_pref("toolkit.scrollbox.horizontalScrollDistance", 6);
-user_pref("toolkit.scrollbox.verticalScrollDistance", 2);
-*/
-
-
-/* interval for saving session data to restore if firefox crashes.
- * default is 15000 milliseconds  (15 seconds) which is too short. ***/
-user_pref("browser.sessionstore.interval", 100000);
-
-/* fix context menu clicking the first item ***/
+/* fix context menu clicking the first item */
 user_pref("ui.context_menus.after_mouseup", true);
-
-/* disable animations */
-user_pref("toolkit.cosmeticAnimations.enabled", false);
 
 /* disable hold to show context menu */
 user_pref("ui.click_hold_context_menus", false);
 
+/* disable animations */
+user_pref("toolkit.cosmeticAnimations.enabled", false);
 
 /* ==============================
  * ===   privacy/annoyances   ===
  * ============================== */
 
-/***** new tab / home page *****/
-
-/* 0102: set START page (0=blank, 1=home, 2=last visited page, 3=resume previous session)
- * [NOTE] Session Restore is not used in PB mode (0110) and is cleared with history (2803, 2804)
- * [SETTING] General>Startup>Restore previous session ***/
-   // user_pref("browser.startup.page", 0); // [HIDDEN PREF]
-
-/* 0103: set HOME+NEWWINDOW page
- * about:home=Activity Stream (default, see 0105), custom URL, about:blank
- * [SETTING] Home>New Windows and Tabs>Homepage and new windows ***/
-   // user_pref("browser.startup.homepage", "about:blank");
-
-/* 0104: set NEWTAB page
- * true=Activity Stream (default, see 0105), false=blank page
- * [SETTING] Home>New Windows and Tabs>New tabs ***/
-   // user_pref("browser.newtabpage.enabled", false);
-   // user_pref("browser.newtab.preload", false);
-
-/***** end of new tab / home page *****/
-
-/* enable userChrome/userContent ***/
+/* enable userChrome/userContent */
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
-/* do not show about:config warning ***/
+/* do not show about:config warning */
 user_pref("browser.aboutConfig.showWarning", false);
 
-/* 0101: disable default browser check
- * [SETTING] General>Startup>Always check if Firefox is your default browser ***/
+/* disable default browser check */
 user_pref("browser.shell.checkDefaultBrowser", false);
 
-/* 0105: disable Activity Stream stuff (AS)
- * AS is the default homepage/newtab in FF57+, based on metadata and browsing behavior.
- *    **NOT LISTING ALL OF THESE: USE THE PREFERENCES UI**
- * [SETTING] Home>Firefox Home Content>...  to show/hide what you want ***/
-/* 0105a: disable Activity Stream telemetry ***/
+/* disable Activity Stream stuff */
+user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.newtab.preload", false);
 user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
 user_pref("browser.newtabpage.activity-stream.telemetry", false);
-/* 0105b: disable Activity Stream Snippets
- * Runs code received from a server (aka Remote Code Execution) and sends information back to a metrics server
- * [1] https://abouthome-snippets-service.readthedocs.io/ ***/
-user_pref("browser.newtabpage.activity-stream.feeds.snippets", false);
-user_pref("browser.newtabpage.activity-stream.asrouter.providers.snippets", "{}");
-/* 0105c: disable Activity Stream Top Stories, Pocket-based and/or sponsored content ***/
+user_pref("browser.newtabpage.activity-stream.feeds.snippets", false); // [DEFAULT: false FF89+]
 user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
 user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false);
 user_pref("browser.newtabpage.activity-stream.showSponsored", false);
 user_pref("browser.newtabpage.activity-stream.feeds.discoverystreamfeed", false); // [FF66+]
-/* disable pocket ***/
-user_pref("extensions.pocket.enabled", false);
-/* 0105d: disable Activity Stream recent Highlights in the Library [FF57+] ***/
-   // user_pref("browser.library.activity-stream.enabled", false);
-/* 0105e: clear default topsites
- * [NOTE] This does not block you from adding your own ***/
 user_pref("browser.newtabpage.activity-stream.default.sites", "");
 
-/* 0210: set preferred language for displaying web pages
- * [TEST] https://addons.mozilla.org/about ***/
-user_pref("intl.accept_languages", "en-US, en");
-/* 0309: disable sending Flash crash reports ***/
-user_pref("dom.ipc.plugins.flash.subprocess.crashreporter.enabled", false);
-/* 0310: disable sending the URL of the website where a plugin crashed ***/
-user_pref("dom.ipc.plugins.reportCrashURL", false);
-/* 0320: disable about:addons' Recommendations pane (uses Google Analytics) ***/
-user_pref("extensions.getAddons.showPane", false); // [HIDDEN PREF]
-/* 0321: disable recommendations in about:addons' Extensions and Themes panes [FF68+] ***/
-user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+/* disable pocket */
+user_pref("extensions.pocket.enabled", false);
+
+/* use mozilla's geolocation provider */
+user_pref("geo.provider.network.url", "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%");
 
 /* 0204: disable using the OS's geolocation service ***/
-user_pref("geo.provider.ms-windows-location", false); // [WINDOWS]
-user_pref("geo.provider.use_corelocation", false); // [MAC]
-user_pref("geo.provider.use_gpsd", false); // [LINUX]
+user_pref("geo.provider.ms-windows-location", false); // Windows
+user_pref("geo.provider.use_corelocation", false); // Mac
+user_pref("geo.provider.use_gpsd", false); // Linux
 
-/* 0330: disable telemetry
- * the pref (.unified) affects the behaviour of the pref (.enabled)
- * IF unified=false then .enabled controls the telemetry module
- * IF unified=true then .enabled ONLY controls whether to record extended data
- * so make sure to have both set as false
- * [NOTE] FF58+ 'toolkit.telemetry.enabled' is now LOCKED to reflect prerelease
- * or release builds (true and false respectively), see [2]
- * [1] https://firefox-source-docs.mozilla.org/toolkit/components/telemetry/telemetry/internals/preferences.html
- * [2] https://medium.com/georg-fritzsche/data-preference-changes-in-firefox-58-2d5df9c428b5 ***/
+/* disable region updates */
+user_pref("browser.region.network.url", "");
+user_pref("browser.region.update.enabled", false);
+
+/* set preferred website display language */
+user_pref("intl.accept_languages", "en-US, en");
+
+/* disable firefox auto-installing it's updates */
+user_pref("app.update.auto", false);
+
+/* disable bullshit search-engine updates */
+user_pref("browser.search.update", false);
+
+/* disable annoying recommendations */
+user_pref("extensions.getAddons.showPane", false);
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+
+/* disable telemetry */
 user_pref("toolkit.telemetry.unified", false);
-user_pref("toolkit.telemetry.enabled", false); // see [NOTE] above FF58+
+user_pref("toolkit.telemetry.enabled", false);
 user_pref("toolkit.telemetry.server", "data:,");
 user_pref("toolkit.telemetry.archive.enabled", false);
-user_pref("toolkit.telemetry.newProfilePing.enabled", false); // [FF55+]
-user_pref("toolkit.telemetry.shutdownPingSender.enabled", false); // [FF55+]
-user_pref("toolkit.telemetry.updatePing.enabled", false); // [FF56+]
-user_pref("toolkit.telemetry.bhrPing.enabled", false); // [FF57+] Background Hang Reporter
-user_pref("toolkit.telemetry.firstShutdownPing.enabled", false); // [FF57+]
-/* 0331: disable Telemetry Coverage
- * [1] https://blog.mozilla.org/data/2018/08/20/effectively-measuring-search-in-firefox/ ***/
-user_pref("toolkit.telemetry.coverage.opt-out", true); // [HIDDEN PREF]
-user_pref("toolkit.coverage.opt-out", true); // [FF64+] [HIDDEN PREF]
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+user_pref("toolkit.telemetry.coverage.opt-out", true);
+user_pref("toolkit.coverage.opt-out", true);
 user_pref("toolkit.coverage.endpoint.base", "");
-/* 0340: disable Health Reports
- * [SETTING] Privacy & Security>Firefox Data Collection & Use>Allow Firefox to send technical... data ***/
 user_pref("datareporting.healthreport.uploadEnabled", false);
-/* 0341: disable new data submission, master kill switch [FF41+]
- * If disabled, no policy is shown or upload takes place, ever
- * [1] https://bugzilla.mozilla.org/1195552 ***/
 user_pref("datareporting.policy.dataSubmissionEnabled", false);
-/* 0342: disable Studies (see 0503)
- * [SETTING] Privacy & Security>Firefox Data Collection & Use>Allow Firefox to install and run studies ***/
 user_pref("app.shield.optoutstudies.enabled", false);
-/* 0343: disable personalized Extension Recommendations in about:addons and AMO [FF65+]
- * [NOTE] This pref has no effect when Health Reports (0340) are disabled
- * [SETTING] Privacy & Security>Firefox Data Collection & Use>Allow Firefox to make personalized extension recommendations
- * [1] https://support.mozilla.org/kb/personalized-extension-recommendations ***/
 user_pref("browser.discovery.enabled", false);
-/* 0350: disable Crash Reports ***/
 user_pref("breakpad.reportURL", "");
-user_pref("browser.tabs.crashReporting.sendReport", false); // [FF44+]
-user_pref("browser.crashReports.unsubmittedCheck.enabled", false); // [FF51+]
-/* 0351: disable backlogged Crash Reports
- * [SETTING] Privacy & Security>Firefox Data Collection & Use>Allow Firefox to send backlogged crash reports  ***/
-user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false); // [FF58+]
-/* 0390: disable Captive Portal detection
- * [1] https://www.eff.org/deeplinks/2017/08/how-captive-portals-interfere-wireless-security-and-privacy
- * [2] https://wiki.mozilla.org/Necko/CaptivePortal ***/
+user_pref("browser.tabs.crashReporting.sendReport", false);
+user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
 user_pref("captivedetect.canonicalURL", "");
-user_pref("network.captive-portal-service.enabled", false); // [FF52+]
-/* 0391: disable Network Connectivity checks [FF65+]
- * [1] https://bugzilla.mozilla.org/1460537 ***/
+user_pref("network.captive-portal-service.enabled", false);
 user_pref("network.connectivity-service.enabled", false);
 
-/* 0410: disable SB (Safe Browsing)
- * [WARNING] Do this at your own risk! These are the master switches.
- * [SETTING] Privacy & Security>Security>... "Block dangerous and deceptive content" ***/
-user_pref("browser.safebrowsing.malware.enabled", false);
-user_pref("browser.safebrowsing.phishing.enabled", false);
-/* 0411: disable SB checks for downloads (both local lookups + remote)
- * This is the master switch for the safebrowsing.downloads* prefs (0412, 0413)
- * [SETTING] Privacy & Security>Security>... "Block dangerous downloads" ***/
-user_pref("browser.safebrowsing.downloads.enabled", false);
-/* 0412: disable SB checks for downloads (remote)
- * To verify the safety of certain executable files, Firefox may submit some information about the
- * file, including the name, origin, size and a cryptographic hash of the contents, to the Google
- * Safe Browsing service which helps Firefox determine whether or not the file should be blocked
- * [SETUP-SECURITY] If you do not understand this, or you want this protection, then override it ***/
+/* enable blocklists */
+user_pref("extensions.blocklist.enabled", true);
+
+/* disable checking downloaded files */
 user_pref("browser.safebrowsing.downloads.remote.enabled", false);
 user_pref("browser.safebrowsing.downloads.remote.url", "");
+user_pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", false);
+user_pref("browser.safebrowsing.downloads.remote.block_uncommon", false);
 
-/* 0503: disable Normandy/Shield [FF60+]
- * Shield is an telemetry system (including Heartbeat) that can also push and test "recipes"
- * [1] https://wiki.mozilla.org/Firefox/Shield
- * [2] https://github.com/mozilla/normandy ***/
+/* disable some system addons */
 user_pref("app.normandy.enabled", false);
 user_pref("app.normandy.api_url", "");
+user_pref("browser.ping-centre.telemetry", false);
 
-/* 0701: disable IPv6
- * IPv6 can be abused, especially with MAC addresses, and they do not play nice with VPNs. That's
- * even assuming your ISP and/or router and/or website can handle it. Sites will fall back to IPv4
- * [STATS] Firefox telemetry (June 2020) shows only 5% of all connections are IPv6
- * [NOTE] This is just an application level fallback. Disabling IPv6 is best done at an
- * OS/network level, and/or configured properly in VPN setups. If you are not masking your IP,
- * then this won't make much difference. If you are masking your IP, then it can only help.
- * [TEST] https://ipleak.org/
- * [1] https://github.com/arkenfox/user.js/issues/437#issuecomment-403740626
- * [2] https://www.internetsociety.org/tag/ipv6-security/ (see Myths 2,4,5,6) ***/
+/* disable autofill */
+user_pref("extensions.formautofill.addresses.enabled", false);
+user_pref("extensions.formautofill.available", "off");
+user_pref("extensions.formautofill.creditCards.available", false);
+user_pref("extensions.formautofill.creditCards.enabled", false);
+user_pref("extensions.formautofill.heuristics.enabled", false);
+
+/* disable more telemetry bullshit */
+user_pref("extensions.webcompat-reporter.enabled", false);
+
+/* disable predictions and prefetching */
+user_pref("network.prefetch-next", false);
+user_pref("network.dns.disablePrefetch", true);
+user_pref("network.predictor.enabled", false);
+user_pref("network.http.speculative-parallel-limit", 0);
+
+/* disable IPv6 which can be abused and leak data */
 user_pref("network.dns.disableIPv6", true);
 
-/* 0704: enforce the proxy server to do any DNS lookups when using SOCKS
- * e.g. in Tor, this stops your local DNS server from knowing your Tor destination
- * as a remote Tor node will handle the DNS request
- * [1] https://trac.torproject.org/projects/tor/wiki/doc/TorifyHOWTO/WebBrowsers ***/
-user_pref("network.proxy.socks_remote_dns", true);
+/* disable a potential proxy bypass */
+user_pref("network.gio.supported-protocols", "");
+
+/* disable apparently dangerous domain guessing */
+user_pref("browser.fixup.alternate.enabled", false);
+
+/* display the whole url */
+user_pref("browser.urlbar.trimURLs", false);
+
+/* disable live search suggestions and address bar leakages */
+user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.urlbar.suggest.searches", false);
+user_pref("browser.urlbar.speculativeConnect.enabled", false);
+user_pref("browser.urlbar.dnsResolveSingleWordsAfterSearch", 0);
+
+/* more suggestion settings */
+user_pref("browser.urlbar.suggest.history", true);
+user_pref("browser.urlbar.suggest.bookmark", true);
+user_pref("browser.urlbar.suggest.openpage", true);
+user_pref("browser.urlbar.suggest.topsites", false);
+
+/* don't ask to save passwords */
+user_pref("signon.rememberSignons", false);
+
+/* don't write cache to disk in private browsing */
+user_pref("browser.privatebrowsing.forceMediaMemoryCache", true);
+
+/* enforce TLS 1.0 and 1.1 downgrades as session only */
+user_pref("security.tls.version.enable-deprecated", false);
+
+/* disable TLS1.3 0-RTT (round-trip time) */
+user_pref("security.tls.enable_0rtt_data", false);
+
+/* display warning on the padlock for "broken security" */
+user_pref("security.ssl.treat_unsafe_negotiation_as_broken", true);
+
+/* display advanced information on Insecure Connection warning pages */
+user_pref("browser.xul.error_pages.expert_bad_cert", true);
+
+/* display "insecure" icon and "Not Secure" text on HTTP sites */
+user_pref("security.insecure_connection_text.enabled", true);
+
+/* disable graphite which has many security issues */
+user_pref("gfx.font_rendering.graphite.enabled", false);
+
+/* send less info on cross origin */
+user_pref("network.http.referer.XOriginTrimmingPolicy", 2);
+
+/* do not track */
+user_pref("privacy.donottrackheader.enabled", true);
+
+/* disable all DRM content (EME: Encryption Media Extension) */
+user_pref("media.eme.enabled", false);
+
+/* block popups */
+user_pref("dom.disable_open_during_load", true);
+
+/* limit events that can cause a popup */
+user_pref("dom.popup_allowed_events", "click dblclick mousedown pointerdown");
+
+/* disable autoplay of HTML5 media */
+user_pref("media.autoplay.default", 5);
+
+/* prevent scripts from moving and resizing open windows */
+user_pref("dom.disable_window_move_resize", true);
+
+/* disable push notifications */
+user_pref("dom.push.enabled", false);
+
+/* disable clipboard commands (cut/copy) from "non-privileged" content */
+user_pref("dom.allow_cut_copy", false);
+
+/* disable insecure asm.js */
+user_pref("javascript.options.asmjs", false);
+
+/* disable accessiblity */
+user_pref("accessibility.force_disabled", 1);
+
+/* disable beacon analytics */
+user_pref("beacon.enabled", false);
+
+/* remove temp files opened with an external application */
+user_pref("browser.helperApps.deleteTempFileOnExit", true);
+
+/* disable page thumbnail collection */
+user_pref("browser.pagethumbnails.capturing_disabled", true);
+
+/* disable some devtools */
+user_pref("devtools.chrome.enabled", false);
+user_pref("devtools.debugger.remote-enabled", false);
+
+/* remove special permissions for mozilla domains */
+user_pref("permissions.manager.defaultsUrl", "");
+
+/* enable an important security feature */
+user_pref("security.csp.enable", true);
+
+/* block all third party cookies */
+user_pref("network.cookie.cookieBehavior", 1);
+user_pref("browser.contentblocking.category", "custom");
+
+/* disable social tracking */
+user_pref("privacy.trackingprotection.socialtracking.enabled", true);
+
+/* resist fingerprinting (disable if causing weird behaviour in websites) */
+user_pref("privacy.resistFingerprinting", true);
+
+/* disable some bullshit annoyances */
+user_pref("browser.startup.homepage_override.mstone", "ignore");
+user_pref("browser.messaging-system.whatsNewPanel.enabled", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
