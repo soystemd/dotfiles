0b1f098fb5d5c8966ef237283a800faad3e37ce9
ehsan gn <ehsangn@protonmail.ch>
2021-06-26 20:41:16 +0430

    add gnupg's directory to mkdir in install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@
 # that goes in there, will end up amongst our dotfiles, which is not ok.
 # read more on folding in stow's manpage.
 mkdir -p ~/.local/bin ~/.local/share/applications ~/.local/share/wall \
-    ~/.local/sv/run ~/.config/tremc ~/.config/kicad ~/.config/safeeyes \
-    ~/.config/pulse ~/.config/gtk-2.0 ~/.config/gtk-3.0 \
+    ~/.local/share/gnupg ~/.local/sv/run ~/.config/tremc ~/.config/kicad \
+    ~/.config/safeeyes ~/.config/pulse ~/.config/gtk-2.0 ~/.config/gtk-3.0 \
     ~/.config/transmission-daemon
 
 # install all the dotfile packages one by one.
