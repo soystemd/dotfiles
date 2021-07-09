09d423b397889297c781b113f414e79375728698
ehsan gn <ehsangn@protonmail.ch>
2021-07-09 23:47:04 +0430

    dwm and dwmbar now get their colors from xresources

--- pywal/.config/wal/templates/dwm.h
+++
@@ -1,9 +0,0 @@
-#define PYWAL
-static const char col_null[] = "#000000";
-static const char common_bg[] = "{background}";
-static const char norm_fg[] = "{color2}";
-static const char norm_border[] = "{background}";
-static const char sel_fg[] = "{color6}";
-static const char sel_border[] = "{color6}";
-static const char sel_float[] = "{color9}";
-static const char title_fg[] = "{color2}";
--- pywal/.config/wal/templates/dwmbar.h
+++
@@ -1,3 +0,0 @@
-#define PYWAL
-static const std::string dim("{color2}");
-static const std::string bright("{color6}");
